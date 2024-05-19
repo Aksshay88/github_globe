cfbf5f6aff8325a437169984c5cedba0e5f2d150
Aksshay88 <aksshaybala8846@gmail.com>
2024-05-19 10:13:14 +0530

    gitglobe added :)

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.css";
+import { GlobeDemo } from "./components/world";
+
+function App() {
+  return (
+    <section>
+      <GlobeDemo />
+    </section>
+  );
+}
+
+export default App;
